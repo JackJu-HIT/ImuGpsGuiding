31c7019ed456212f584a3e6a7104a6b85d58ca5f
Juchunyu <1101926054@qq.com>
2020-07-22 11:08:01 +0800

    内容修正了

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # ImuGpsGuiding
 利用IMU和GPS从指定起点到指定终点，基于ROS框架。
 # 使用的均是真实的传感器，以实际工程为导向
+进行了修正，但是还没正式测试
+代码中订阅了
+imu：/imu
+gps： /fix
+lidar：/scan
+发布了：
+/cmd_vel
+  
--- src/IMU-GPS-Guiding.cpp
+++ src/IMU-GPS-Guiding.cpp
@@ -4,7 +4,6 @@
  * HIT
  * 20200712
  */
- 
 #include <ros/ros.h>
 #include "sensor_msgs/Imu.h"
 #include "sensor_msgs/NavSatFix.h"
@@ -12,9 +11,8 @@
 #include <math.h>
 #include "sensor_msgs/LaserScan.h"
 #include <boost/thread.hpp>
+//#include<subscriber.h>
 using namespace std;
-
-
 
 class IMU_GPS_Guiding
 {
@@ -28,22 +26,22 @@ private:
     double gps_x;
     double gps_y;
     /*****初始位置姿态设置和目标点设置*****/
-    double x;
-    double y;//起点坐标
-    double theta;//初始航向角
-    double x_goal;
-    double y_goal;
+    double x=0;
+    double y=0;//起点坐标
+    double theta=3.1415926/2;//初始航向角
+    double x_goal=10;
+    double y_goal=10;
     double t=0.02;//采样频率，imu发布话题的频率为50HZ
-    double L;//两个推进器间的距离
+    double L=4;//两个推进器间的距离
     //pid控制器参数设置
         //航向控制参数设置
-    double k;
-    double ki;
-    double kd;
-    double error_sum;
-    double pre_error;
+    double k=10;
+    double ki=0.1;
+    double kd=0.1;
+    double error_sum=0;
+    double pre_error=0;
     //距离控制参数设置
-    double k2;
+    double k2=0.1;
     //是否初始化
     bool isinitized=false;
     double w;
@@ -51,7 +49,6 @@ private:
     double v_y;
 
 public:
-
     IMU_GPS_Guiding(/* args */);
     void imuInfoCallback(const sensor_msgs::Imu::ConstPtr& msg);
     void gpsInfoCallback(const sensor_msgs::NavSatFix::ConstPtr& msg);
@@ -59,14 +56,12 @@ public:
     ~IMU_GPS_Guiding();
     void initized_and_parameter_setting();
 };
-
 IMU_GPS_Guiding::IMU_GPS_Guiding(/* args */)
 {
     Twist_info_pub=n.advertise<geometry_msgs::Twist>("/cmd_vel",1000);
     gps_info_sub=n.subscribe("/fix",10,&IMU_GPS_Guiding::gpsInfoCallback,this);
     imu_info_sub=n.subscribe("/imu",10,&IMU_GPS_Guiding::imuInfoCallback,this);
     lidar_info_sub=n.subscribe("/scan",10,&IMU_GPS_Guiding::scanInfoCallback,this);
-
 }
 
 IMU_GPS_Guiding::~IMU_GPS_Guiding()
@@ -91,25 +86,44 @@ void IMU_GPS_Guiding::imuInfoCallback(const sensor_msgs::Imu::ConstPtr& msg)
 	//msg->linear_acceleration.x, msg->linear_acceleration.y,x);
    // cout<<"imu:"<<msg->linear_acceleration.x<<endl;
    // cout<<"test:"<<x<<endl;
-if(!isinitized)
+if(!this->isinitized)
 {
-  /*****初始位置姿态设置和目标点设置*****/
-    this->x=0;;
-    this->y=0;//起点坐标
-    this->theta=3.1415926/2;//初始航向角
-    this->x_goal=10;
-    this->y_goal=10;
-    ///this->T=0.02;//采样频率，imu发布话题的频率为50HZ
-    this->L=4.0;//两个推进器间的距离
-    //pid控制器参数设置
-        //航向控制参数设置
-    this->k=10;
-    this->ki=0.1;
-    this->kd=0.1;
-    this->pre_error=0;
-    //距离控制参数设置
-    this->k2=10;
   cout<<"初始化中"<<endl;
+//航向角度闭环pid控制
+    double theta_error;
+    theta_error=this->theta-atan((this->y_goal-this->y)/(this->x_goal-this->x));//防止分母为零。
+    this->error_sum=this->error_sum+theta_error;
+    this->w=-this->k*theta_error+this->ki*this->error_sum+this->kd*(theta_error-this->pre_error);
+    this->pre_error=theta_error;
+
+//速度pid控制
+double dist=sqrt((this->x-this->x_goal)*(this->x-this->x_goal)+(this->y-this->y_goal)*(this->y-this->y_goal));
+double v=k2*dist;
+this->v_x=v*cos(this->theta);
+this->v_y=v*sin(this->theta);
+this->isinitized=true;
+}
+else
+{
+    double angular_z=(msg->angular_velocity.z)-0.01;//0.01是对imu偏差的补偿
+    this->theta=this->theta+angular_z*this->t;//航向角推算
+    /*****位置推算*****/
+    double  x_acc_rece=(msg->linear_acceleration.x)-0.0337;//0.3是我补偿
+    double  y_acc_rece=(msg->linear_acceleration.y)-0.7182;//0.46是我补偿
+    //坐标变换：将加速度计的信息从小车坐标系转换到大地坐标系
+    double x_acc=x_acc_rece*cos(this->theta)-y_acc_rece*sin(this->theta);
+    double y_acc=x_acc_rece*sin(this->theta)+y_acc_rece*cos(this->theta);
+    
+    //开始推算坐标
+    this->v_x=this->v_x+x_acc*this->t;       
+    this->v_y=this->v_y+y_acc*this->t;
+    this->x=this->x+this->v_x*this->t;//推算出的x坐标
+    this->y=this->y+this->v_y*this->t;//推算出的y坐标
+    cout<<"ax:"<<x_acc<<endl;
+    cout<<"T:"<<this->t<<endl;
+    cout<<endl;
+    cout<<"初始化已经结束"<<endl;
+    cout<<"gpx_x:"<<this->gps_x<<"gps_y"<<this->gps_y<<endl;
 //航向角度闭环pid控制
     double theta_error;
     theta_error=this->theta-atan((this->y_goal-this->y)/(this->x_goal-this->x));
@@ -123,40 +137,21 @@ double dist=sqrt((this->x-this->x_goal)*(this->x-this->x_goal)+(this->y-this->y_
 double v=k2*dist;
 this->v_x=v*cos(this->theta);
 this->v_y=v*sin(this->theta);
-this->isinitized=true;
 }
-else
+
+
+
+/***设定运动停止*****/
+double dist_compare=sqrt((this->x-this->x_goal)*(this->x-this->x_goal)+(this->y-this->y_goal)*(this->y-this->y_goal));
+if(dist_compare<1)
 {
-    double angular_z=(msg->angular_velocity.z);
-    this->theta=this->theta+angular_z*this->t;//航向角推算
-    /*****位置推算*****/
-    double  x_acc=(msg->linear_acceleration.x);
-    double  y_acc=(msg->linear_acceleration.y);
-    this->v_x=this->v_x+x_acc*this->t;
-    this->v_y=this->v_y+y_acc*this->t;
-   this-> x=this->x+this->v_x*this->t;//推算出的x坐标
-    this->y=this->y+this->v_y*this->t;//推算出的y坐标
-    cout<<"ax:"<<x_acc<<endl;
-    cout<<"T:"<<this->t<<endl;
-    cout<<endl;
-    cout<<"初始化已经结束"<<endl;
-    cout<<"gpx_x:"<<this->gps_x<<"gps_y"<<this->gps_y<<endl;
-//航向角度闭环pid控制
-    double theta_error;
-    theta_error=this->theta-atan((this->y_goal-this->y)/(this->x_goal-this->x));
-    this->error_sum=this->error_sum+theta_error;
-    this->w=-this->k*theta_error;//+this->ki*this->error_sum+this->kd*(theta_error-this->pre_error);
-    this->pre_error=theta_error;
-
-   //速度pid控制
-   
-double dist=sqrt((this->x-this->x_goal)*(this->x-this->x_goal)+(this->y-this->y_goal)*(this->y-this->y_goal));
-double v=k2*dist;
-this->v_x=v*cos(this->theta);
-this->v_y=v*sin(this->theta);
+ this->v_x=0;
+ this->v_y=0;
+ this->w=0;
+ imu_info_sub.shutdown();//当到达目标，不会去订阅imu话题。
 }
 
- 
+
   // ros::init(argc, argv, "Twist_publisher");
   /*
  ros::NodeHandle n;
@@ -164,13 +159,15 @@ this->v_y=v*sin(this->theta);
 */
  geometry_msgs::Twist vel_pub;
 
-     vel_pub.angular.z=w;
-     vel_pub.linear.x=v_x;
-     vel_pub.linear.y=v_y;
+     vel_pub.angular.z=this->w;
+     vel_pub.linear.x=this->v_x;
+     vel_pub.linear.y=this->v_y;
      Twist_info_pub.publish(vel_pub);
     // Twist_info_pub.publish(vel_pub);
     cout<<"x:"<<this->x<<endl;
     cout<<"y:"<<this->y<<endl;
+    cout<<"v_x"<<this->v_x<<endl;
+    cout<<"v_y"<<this->v_y<<endl;
     cout<<"theta:"<<this->theta<<endl;
    
 }
